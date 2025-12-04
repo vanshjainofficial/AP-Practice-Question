45ac3088620117ff3265545735346d9b1c5e55ce
Vansh Jain <95092012+vanshjainofficial@users.noreply.github.com>
2025-12-04 16:50:48 +0530

    Update index.js

--- Easy 1/index.js
+++ Easy 1/index.js
@@ -1,5 +1,9 @@
-const fs = require("fs");
+const fs = require('fs');
+const path = require('path');
 
 function writeDataFile() {
-
+  const filePath = path.join(__dirname, 'file.txt'); 
+  fs.writeFileSync(filePath, 'We will not miss the exam next time');
 }
+
+writeDataFile();
